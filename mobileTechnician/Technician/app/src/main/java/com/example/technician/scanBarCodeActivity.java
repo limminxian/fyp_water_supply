eb0de096980fd09aa1667280bfdd8a1051e9017a
hasnaruhi <hasna.ruhi16@gmail.com>
2023-02-08 18:49:54 +0800

    heroku 2nd database update

---
+++ mobileTechnician/Technician/app/src/main/java/com/example/technician/scanBarCodeActivity.java
@@ -0,0 +1,14 @@
+package com.example.technician;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class scanBarCodeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scan_bar_code);
+    }
+}
